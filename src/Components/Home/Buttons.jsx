9feb64b91b1a169a1c7770e0bd35ca5614f1927e
Batool Fatima <batoolfatima2135@gmail.com>
2023-09-26 17:07:48 +0500

    Add main page components

---
+++ src/Components/Home/Buttons.jsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+import React from 'react';
+
+export default function Buttons() {
+  return (
+    <div className="flex mt-4">
+      <a type="button" href="./Resume.pdf" target="_blank" className="mx-3 border rounded border-white  text-xs lg:text-sm hover:border-gray-300 hover:shadow p-2.5">Download Resume</a>
+      <Link href="/search" type="button" className="mx-3  border rounded border-white  text-xs lg:text-sm hover:border-gray-300 hover:shadow p-2.5">Who am I?</Link>
+    </div>
+  );
+}
---
+++ src/Components/Home/Heading.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Heading() {
+  return (
+
+    <h1 className=" md:text-6xl lg:text-7xl text-3xl font-medium tracking-tighter text-center">
+      <span className="text-blue-500">G</span>
+      <span className="text-red-500">o</span>
+      <span className="text-yellow-500">o</span>
+      <span className="text-blue-500">g</span>
+      <span className="text-green-600">l</span>
+      <span className="text-red-500">e</span>
+      <span> </span>
+      <span className="text-blue-500">B</span>
+      <span className="text-red-500">a</span>
+      <span className="text-blue-500">t</span>
+      <span className="text-red-500">o</span>
+      <span className="text-yellow-500">o</span>
+      <span className="text-green-600">l</span>
+    </h1>
+  );
+}
