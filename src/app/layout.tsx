3ed079157a1ddd468caf69eb66429e4cab22e835
Batool Fatima <batoolfatima2135@gmail.com>
2023-10-06 01:51:04 +0500

    Add project provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import ProjectProvider from './projectProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}><ProjectProvider>{children}</ProjectProvider></body>
     </html>
   )
 }
