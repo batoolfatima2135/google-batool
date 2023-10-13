16a3df624ce74076255d3cc8acf419526f0936fc
Batool Fatima <batoolfatima2135@gmail.com>
2023-10-13 20:06:39 +0500

    Add topbar

--- src/Components/Search/Topbar.jsx
+++ src/Components/Search/Topbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function Topbar() {
   return (
-    <div className="flex lg:mx-52 ">
+    <div className="flex lg:mx-52 overflow-auto">
       <Link href="/images" className="p-3  text-sm rounded-full border  lg:mx-1 hover:bg-gray-100">Images</Link>
       <Link href="/images" className="p-3 text-sm rounded-full border mx-0.5 lg:mx-1 hover:bg-gray-100">Videos</Link>
       <Link href="/images" className="p-3 text-sm rounded-full border mx-0.5 lg:mx-1 hover:bg-gray-100">Articles</Link>
