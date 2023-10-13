b6795099341ce2c69b6b22f29a9e8d2574859edd
Batool Fatima <batoolfatima2135@gmail.com>
2023-10-13 20:06:48 +0500

    Add video page / tab

---
+++ src/app/videos/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Header from '../../Components/Search/Header';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Batool Fatima',
+  description:
+    'I am Batool Fatima, a Front-End developer, ready to embark on new challenges. With a solid foundation in Front-End technologies like JavaScript, React, and Redux I excel at creating efficient Web applications.Frontend: JavaScript, React, Redux, BootstrapTools & Methods: Git, GitHub, Chrome Dev Tools.Professional: Remote Pair-Programing, Teamwork, Mentoring',
+};
+
+export default function Layout(props: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Header />
+      <main>
+        {props.children}
+       
+
+      </main>
+    </>
+  );
+}
