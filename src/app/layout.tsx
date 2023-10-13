7aaa16c76c8111d1b99391256353538246687ee1
Batool Fatima <batoolfatima2135@gmail.com>
2023-10-13 20:05:15 +0500

    Add main page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import ProjectProvider from './projectProvider'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import ProjectProvider from './projectProvider';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Batool Fatima',
   description: 'I am Batool Fatima, a Front-End developer, ready to embark on new challenges. With a solid foundation in Front-End technologies like JavaScript, React, and Redux I excel at creating efficient Web applications.Frontend: JavaScript, React, Redux, BootstrapTools & Methods: Git, GitHub, Chrome Dev Tools.Professional: Remote Pair-Programing, Teamwork, Mentoring',
-}
+};
 
 export default function RootLayout({
   children,
@@ -19,5 +19,5 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}><ProjectProvider>{children}</ProjectProvider></body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,22 +5,24 @@ import Buttons from '../Components/Home/Buttons';
 import Footer from '../Components/Home/Footer';
 
 export default function Home() {
- 
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <div className="flex-grow">
         <main className="flex flex-col items-center justify-between">
           <Heading />
-          <div className='lg:mt-8 mt-3 lg:w-1/2 w-3/4'>
-              <Input />
+          <div className="lg:mt-8 mt-3 lg:w-1/2 w-3/4">
+            <Input />
           </div>
-          
+
           <Buttons />
-          <p className=" text-xs lg:text-sm m-3">Google offered in: <a href="./Resume.pdf" target="_blank" className='text-blue-900'>Batool</a></p>
+          <p className=" text-xs lg:text-sm m-3">
+            Google offered in:
+            <a href="./Resume.pdf" target="_blank" className="text-blue-900">Batool</a>
+          </p>
         </main>
       </div>
-      <p className=' text-xs lg:text-sm text-gray-900 p-5'>Pakistan</p>
+      <p className=" text-xs lg:text-sm text-gray-900 p-5">Pakistan</p>
       <Footer />
     </div>
   );
