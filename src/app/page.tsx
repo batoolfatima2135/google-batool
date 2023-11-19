d05526a476c8c94613e023d43006eaf4d6e145ff
Batool Fatima <batoolfatima2135@gmail.com>
2023-11-19 22:07:08 +0500

    Add top padding to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <div className="flex-grow">
-        <main className="flex flex-col items-center justify-between">
+        <main className="flex flex-col items-center justify-between mt-28">
           <Heading />
           <div className="lg:mt-8 mt-3 lg:w-1/2 w-3/4">
             <Input />
