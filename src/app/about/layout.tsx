d656c06dbc15549aaf1361b0fc875cc55da5b1ef
Batool Fatima <batoolfatima2135@gmail.com>
2023-11-20 14:26:07 +0500

    Adjust margin left for mobile screen

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -11,7 +11,7 @@ export default function Layout({
       <Header />
       <main className="grid gird-cols-8 lg:grid-cols-9">
         <div className=" lg:col-span-1" />
-        <div className="lg:col-span-5 col-span-7 mx-4 md:col-span-5">
+        <div className="lg:col-span-5 col-span-7 mx-5 md:col-span-5">
           {children}
         </div>
         <div className="col-span-3 hidden lg:block md:block">
