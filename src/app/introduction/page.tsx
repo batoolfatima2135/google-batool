075c4acf9592a49b9552e14f8114eab4f229e236
Batool Fatima <batoolfatima2135@gmail.com>
2023-11-21 22:03:54 +0500

    Update intro video

--- src/app/introduction/page.tsx
+++ src/app/introduction/page.tsx
@@ -11,7 +11,7 @@ export default function About() {
     <div className="lg:m-6 m-2">
       <h1 className="text-xl font-bold"> Hey there! 👋 Get ready for a quick intro 🌟</h1>
       <h1 className="text-lg font-bold text-gray-700 my-3"> I hope you enjoy the video!  🎥 </h1>
-      <iframe src="https://www.youtube.com/embed/Fvw-E-pgo4c?si=dXHKprd6zElWNiui" title="Introduction" className="lg:w-full lg:h-96 md:w-full md:h-96" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
+      <iframe src="https://www.youtube.com/embed/F1dPD94uVtc?si=XD5kW0UOdN7Uhzin" title="Introduction" className="lg:w-full lg:h-96 md:w-full md:h-96" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
       <Faq />
     </div>
   );
