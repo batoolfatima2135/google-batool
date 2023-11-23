d0cf3c6eae2acf2fb4b69ab1abb214d078116b57
Batool Fatima <batoolfatima2135@gmail.com>
2023-11-23 23:44:51 +0500

    Add work link to top bar

--- src/Components/Search/Topbar.jsx
+++ src/Components/Search/Topbar.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 export default function Topbar() {
   return (
     <div className="flex lg:mx-52 overflow-auto">
+      <Link href="/work" className="p-3  text-sm rounded-full border  lg:mx-1 hover:bg-gray-100">Projects</Link>
       <Link href="/images" className="p-3  text-sm rounded-full border  lg:mx-1 hover:bg-gray-100">Images</Link>
       <Link href="/videos" className="p-3 text-sm rounded-full border mx-0.5 lg:mx-1 hover:bg-gray-100">Videos</Link>
       <Link href="/about" className="p-3 text-sm mx-0.5 rounded-full border lg:mx-1 hover:bg-gray-100">About</Link>
