bf242dba24fe5ae02b7b240ce4116c095d086720
Batool Fatima <batoolfatima2135@gmail.com>
2023-11-27 17:59:17 +0500

    Update link of project

--- src/Components/Home/Buttons.jsx
+++ src/Components/Home/Buttons.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function Buttons() {
   return (
     <div className="flex mt-4">
-      <Link href="/introduction" type="button" className="mx-3  border rounded border-white  text-xs lg:text-sm hover:border-gray-300 hover:shadow p-2.5">Projects</Link>
+      <Link href="/work" type="button" className="mx-3  border rounded border-white  text-xs lg:text-sm hover:border-gray-300 hover:shadow p-2.5">Projects</Link>
       <a type="button" href="./Batool_Fatima_Resume.pdf" target="_blank" className="mx-3 border rounded border-white  text-xs lg:text-sm hover:border-gray-300 hover:shadow p-2.5">Download Resume</a>
       <Link href="/introduction" type="button" className="mx-3  border rounded border-white  text-xs lg:text-sm hover:border-gray-300 hover:shadow p-2.5">Who am I?</Link>
     </div>
