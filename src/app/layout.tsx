ff005527598788d459dc4c4bab0ac5e8e03a5311
= <=>
2026-07-16 00:28:46 +0200

    Update information

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,18 +7,23 @@ import LinksProvider from './linksProvider.tsx';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Batool Fatima',
-  description: 'I am Batool Fatima, a Front-End developer, ready to embark on new challenges. With a solid foundation in Front-End technologies like JavaScript, React, and Redux I excel at creating efficient Web applications.Frontend: JavaScript, React, Redux, BootstrapTools & Methods: Git, GitHub, Chrome Dev Tools.Professional: Remote Pair-Programing, Teamwork, Mentoring',
+  title: 'Batool Fatima Zaidi',
+  description:
+    'I am Batool Fatima Zaidi, a Software engineer, ready to embark on new challenges. With a solid foundation in Front-End technologies like JavaScript, React, and Redux I excel at creating efficient Web applications.Frontend: JavaScript, React, Redux, BootstrapTools & Methods: Git, GitHub, Chrome Dev Tools.Professional: Remote Pair-Programing, Teamwork, Mentoring',
 };
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}><ProjectProvider><LinksProvider>{children}</LinksProvider></ProjectProvider></body>
+      <body className={inter.className}>
+        <ProjectProvider>
+          <LinksProvider>{children}</LinksProvider>
+        </ProjectProvider>
+      </body>
     </html>
   );
 }
