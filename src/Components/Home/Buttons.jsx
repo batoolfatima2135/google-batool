745c87e7e73b7531a9f01584b0b34f72c841a13f
= <=>
2026-07-20 14:38:00 +0200

    Commenting my video temporarly, will upload new one

--- src/Components/Home/Buttons.jsx
+++ src/Components/Home/Buttons.jsx
@@ -5,9 +5,22 @@ import React from 'react';
 export default function Buttons() {
   return (
     <div className="flex mt-4">
-      <Link href="/work" type="button" className="mx-3  border rounded border-white  text-xs lg:text-sm hover:border-gray-300 hover:shadow p-2.5">Projects</Link>
-      <a type="button" href={resume} target="_blank" className="mx-3 border rounded border-white  text-xs lg:text-sm hover:border-gray-300 hover:shadow p-2.5">Download Resume</a>
-      <Link href="/introduction" type="button" className="mx-3  border rounded border-white  text-xs lg:text-sm hover:border-gray-300 hover:shadow p-2.5">Who am I?</Link>
+      <Link
+        href="/work"
+        type="button"
+        className="mx-3  border rounded border-white  text-xs lg:text-sm hover:border-gray-300 hover:shadow p-2.5"
+      >
+        Projects
+      </Link>
+      <a
+        type="button"
+        href={resume}
+        target="_blank"
+        className="mx-3 border rounded border-white  text-xs lg:text-sm hover:border-gray-300 hover:shadow p-2.5"
+      >
+        Download Resume
+      </a>
+      {/* <Link href="/introduction" type="button" className="mx-3  border rounded border-white  text-xs lg:text-sm hover:border-gray-300 hover:shadow p-2.5">Who am I?</Link> */}
     </div>
   );
 }
--- src/Components/Others/Profile.jsx
+++ src/Components/Others/Profile.jsx
@@ -5,15 +5,15 @@ import { email, resume, socialLinks } from '@/static/links';
 export default function Profile() {
   return (
     <div className="group relative">
-      <a href="/introduction">
-        <Image
-          src="/profile-pic.jpg"
-          alt="profile-pic"
-          className="w-8 h-8 rounded-full mx-1"
-          width={400}
-          height={300}
-        />
-      </a>
+      {/* <a href="/introduction"> */}
+      <Image
+        src="/profile-pic.jpg"
+        alt="profile-pic"
+        className="w-8 h-8 rounded-full mx-1"
+        width={400}
+        height={300}
+      />
+      {/* </a> */}
       <div className="bg-gray-600 text-white   text-xs hidden lg-absolute px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300 right-0">
         <p>
           <a href={socialLinks.linkedIn} target="_blank" rel="noreferrer">
