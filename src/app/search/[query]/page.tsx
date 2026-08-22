2b1fdae7787aae778d8d9806710316f8b8197b0a
= <=>
2026-08-22 11:52:23 +0200

    updated nextjs

--- src/app/search/[query]/page.tsx
+++ src/app/search/[query]/page.tsx
@@ -5,30 +5,35 @@ import { LinkContext } from '@/app/linksProvider.tsx';
 import { motion } from 'framer-motion';
 import Faq from '../../../Components/Others/Faq';
 
-export default function Search({ params }: { params: { query: string } }) {
+export default async function Search({
+  params,
+}: {
+  params: Promise<{ query: string }>;
+}) {
   const AllLinks = useContext(LinkContext);
-  const query = params.query.toLowerCase().split('%20');
-  const Links = AllLinks.filter((link) => query.some((w) => link.keywords.includes(w)));
+  const query = (await params).query.toLowerCase().split('%20');
+  const Links = AllLinks.filter((link) =>
+    query.some((w) => link.keywords.includes(w))
+  );
   return (
-    <motion.div 
+    <motion.div
       className="lg:m-6 m-2"
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -20 }}
       transition={{ duration: 0.3 }}
     >
-
-      {Links && Links.map((link) => (
-        <div key={link.id} className="py-2 my-3">
-          {link.data}
-        </div>
-      ))}
+      {Links &&
+        Links.map((link) => (
+          <div key={link.id} className="py-2 my-3">
+            {link.data}
+          </div>
+        ))}
       {Links.length < 1 && (
-      <div className="text-center py-5">
-        <p>Opss!😔 Nothing found.💔</p>
-        <p> Try again or select option from dropdown</p>
-      </div>
-
+        <div className="text-center py-5">
+          <p>Opss!😔 Nothing found.💔</p>
+          <p> Try again or select option from dropdown</p>
+        </div>
       )}
       <Faq />
     </motion.div>
